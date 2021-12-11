e935db6dda88e2c4b35eac93cb77f625b6519454
Bret Barker <bret@abitrandom.net>
2021-12-11 17:56:56 -0500

    day 10, part 2

--- 10.py
+++ 10.py
@@ -30,6 +30,28 @@ def check(line):
     return 0
 
 
+def fix(line):
+    openers = "([{<"
+    closers = ")]}>"
+    points = [1, 2, 3, 4]
+
+    s = []
+    pts = 0
+    for c in line:
+        if c in openers:
+            s.append(c)
+        elif c in closers:
+            last = s.pop()
+            if last != openers[closers.index(c)]:
+                raise  # should be no more syntax errs
+    while len(s) > 0:
+        last = s.pop()
+        line += closers[openers.index(last)]
+        pts = pts * 5 + points[openers.index(last)]
+    # print("fixed :", line, pts)
+    return pts
+
+
 def doit(fname):
     lines = parse(fname)
     pts = 0
@@ -38,5 +60,23 @@ def doit(fname):
     print("solution: ", pts)
 
 
+def doit2(fname):
+    lines = parse(fname)
+    inc = []
+    for l in lines:
+        pts = check(l)
+        if pts == 0:
+            inc.append(l)
+    pts = 0
+    scores = []
+    for l in inc:
+        scores.append(fix(l))
+    scores.sort()
+    print("solution: ", scores[int(len(scores) / 2)])
+
+
 doit("10-sample.txt")  # 26397
 doit("10.txt")  # 339477
+
+doit2("10-sample.txt")  # 288957
+doit2("10.txt")  # 3049320156

92c62a2215b5dd1abe574d69dafa608d79d539cc
Bret Barker <bret@abitrandom.net>
2021-12-12 14:25:53 -0500

    day 12, messy

---
+++ 12.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+"""
+--- Day 11: Passage Pathing ---
+"""
+
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, nv):
+        self.nv = nv
+        self.graph = defaultdict(list)
+
+    def addEdge(self, a, b):
+        self.graph[a].append(b)
+        if a != "start" and b != "end":  # omit these
+            self.graph[b].append(a)
+
+    def walk(self, a, dest, visited, path):
+        # print(">>>>>>>", a,dest,a in visited)
+        path.append(a)
+        if a.islower():
+            visited[a] = True
+
+        if a == dest:
+            # end, backout
+            self.cnt += 1
+            print("path", path, self.cnt)
+        else:
+            # explore all edges from here
+            for c in self.graph[a]:
+                if not c in visited or not visited[c]:  # or visited['small'] == c:
+                    # print(c,dest)
+                    self.walk(c, dest, visited, path)
+
+        path.pop()
+        visited[a] = False
+
+    def lowerdoubles(self, path):
+        return len(
+            {n: path.count(n) for n in set(path) if n.islower() and path.count(n) > 1}
+        )
+
+    def walk2(self, a, dest, visited, path):
+        path.append(a)
+        print(">>>>>>>", a, dest, a in visited, path, self.cnt)
+        if a.islower():
+            visited[a] = True
+
+        if a == dest:
+            # end, backout
+            self.cnt += 1
+            print("path", path, self.cnt)
+        else:
+            # explore all edges from here
+            for c in self.graph[a]:
+                if c.isupper():
+                    self.walk2(c, dest, visited, path)
+                elif c.islower() and c != "start":
+                    # print("islower", c, visited[c] if c in visited else False, self.lowerdoubles(path))
+                    if (c in visited and visited[c]) and self.lowerdoubles(path) == 0:
+                        # print("walking when visited but not double", c)
+                        self.walk2(c, dest, visited, path)
+                    elif c not in visited or visited[c] == False:
+                        # print("walkign when not visited", c)
+                        self.walk2(c, dest, visited, path)
+
+        # Remove current vertex from path[] and mark it as unvisited
+        path.pop()
+        if a not in path:
+            visited[a] = False
+
+    # Prints all paths from 's' to 'd'
+    def paths(self, a, b, small):
+        visited = {"small": None}
+        paths = []
+        self.cnt = 0
+        if small:
+            self.walk2(a, b, visited, paths)
+        else:
+            self.walk(a, b, visited, paths)
+        return self.cnt
+
+    def printg(self):
+        print(self.graph)
+
+
+def parse(fname):
+    with open(fname) as fp:
+        edges = [(a, b) for a, b in [l.strip().split("-") for l in fp.readlines()]]
+        print(edges)
+    return edges
+
+
+def doit(fname, small=False):
+    edges = parse(fname)
+    graph = Graph(len(edges))
+    for a, b in edges:
+        # print(a,b)
+        graph.addEdge(a, b)
+
+    graph.printg()
+    cnt = graph.paths("start", "end", small)
+    print("solution: ", cnt)
+
+
+# doit("12-sample1.txt")  # 10
+# doit("12-sample2.txt")  # 19
+# doit("12-sample3.txt")  # 226
+# doit("12.txt")  # 5874
+
+# doit("12-sample1.txt", True)  # 36
+# doit("12-sample2.txt", True)  # 103
+doit("12-sample3.txt", True)  # 3509
+# doit("12.txt", True)  # 153592
